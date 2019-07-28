205e3eb6df9b3642582e57362ee5974b1a48db84
Sergio Barrera <sdbeng@gmail.com>
2019-07-28 10:11:30 -0700

    update flake8 config

--- app/weather/views.py
+++ app/weather/views.py
@@ -2,13 +2,16 @@ import requests
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=963e410024d4bea5f9a528a77cd58039'
-    city = 'Las Vegas'
+api = '963e410024d4bea5f9a528a77cd58039'
 
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={api}'
+
+    city = 'Las Vegas'
     res = requests.get(url.format(city))
     print(res.text)
-    json_response = res.json() 
+    json_response = res.json()
 
     # create a dict to retrieve the data
     city_data = {
