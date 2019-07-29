d7481d3966ab7badca73083dacdedb4008e9ff60
Sergio Barrera <sdbeng@gmail.com>
2019-07-28 22:05:01 -0700

    update template

--- app/weather/views.py
+++ app/weather/views.py
@@ -2,25 +2,27 @@ import requests
 from django.shortcuts import render
 
 # Create your views here.
-api = '963e410024d4bea5f9a528a77cd58039'
+URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=963e410024d4bea5f9a528a77cd58039'
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={api}'
-
     city = 'Las Vegas'
-    res = requests.get(url.format(city))
-    print(res.text)
+    res = requests.get(URL.format(city))
+    # print(res.text)
     json_response = res.json()
+    print(json_response)
 
     # create a dict to retrieve the data
     city_data = {
         'city': city,
-        'temperature': json_response['main']['temp'],
+        # 'temperature': json_response['main']['temp'],
         'description': json_response['weather'][0]['description'],
         'icon': json_response['weather'][0]['icon'],
     }
 
     print(city_data)
+    # define context
+    context = {'city_data': city_data}
 
-    return render(request, 'weather/weather.html')
+    # return render(request, 'weather/weather.html')
+    return render(request, 'weather/weather.html', context)
